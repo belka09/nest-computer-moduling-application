5e6fbc11d9782886625259e09e92c6ed3a5d5510
Dmitry Zimnitsky <dmitryft4@gmail.com>
2024-03-03 14:34:58 +0100

    re-setup an application to work on cars report app

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ComputerModule } from 'src/computer/computer.module';
+import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [ComputerModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
